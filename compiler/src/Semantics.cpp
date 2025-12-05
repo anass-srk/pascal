3c8422b03811ec6bda09bfe906ff65fdff720ee5
Anass Srk <anasssrk1@gmail.com>
2025-12-05 23:04:43 +0000

    Const bool support + enum (TYPE_CAT) to store the type of Derived<Type> which is the base object instead of RTTI (faster) + enum values stored redundantly (fast search) + Array (Type) class correction to include index types + block init with basic types (Int,...). Also, functions/procedures stored in same data type (nullable return value), with their signatures stored as types.

--- compiler/src/Semantics.cpp
+++ compiler/src/Semantics.cpp
@@ -3,221 +3,73 @@
 namespace pascal_compiler
 {
 
-Const::Const(std::string_view name, size_t line, size_t col, std::string_view value, Enum *type) : m_name(name), m_line(line), m_col(col), m_type(type) 
-{
-  if(!m_type) 
-    throw SemanticException
-    (
-      SEMANTIC_ERROR::SE_INVALID_TYPE,
-      std::format
-      (
-        "Semantic error: the type of the constant '{}' at ({},{}) is null ?",
-        m_name,m_line,m_col  
-      ),
-      m_line,m_col
-    );
-  
-  const auto it = type->m_values.find(value);
-  if(it == type->m_values.end())
+void Block::init_main_block(Block& block){
+  auto &types = block.m_types;
+  for (auto type : {CONST_CAT::CC_INT, CONST_CAT::CC_REAL, CONST_CAT::CC_CHAR, CONST_CAT::CC_STRING, CONST_CAT::CC_BOOL})
   {
-    throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the type of the constant '{}' at ({},{}) is supposedly the enum {}, but {} isn't one of it's values !",
-          m_name, m_line, m_col, type->to_string(), value 
-        ),
-        m_line, m_col
-      );
+    auto name = CONST_CAT_NAMES[int(type)];
+    types[name] = std::make_shared<Type>(name, 0, 0, TYPE_CAT::TC_BASIC);
   }
+}
 
-  m_ival = it->second;
+Const::Const(std::string_view name, size_t line, size_t col, const EnumValue& value) : m_name(name), m_line(line), m_col(col),
+  m_type(static_cast<const Type*>(value.m_type)) 
+{
+  m_val = value.m_value;
   m_cat = CONST_CAT::CC_ENUM;
 }
 
-template Const::Const(std::string_view name, size_t line, size_t col, Int value, Type *type);
-template Const::Const(std::string_view name, size_t line, size_t col, Real value, Type *type);
-template Const::Const(std::string_view name, size_t line, size_t col, char value, Type *type);
-template Const::Const(std::string_view name, size_t line, size_t col, std::string_view value, Type *type);
-
-template <ConstType T>
-Const::Const(std::string_view name, size_t line, size_t col, T value, Type *type) : m_name(name), m_line(line), m_col(col), m_type(type)
+Const::Const(std::string_view name, size_t line, size_t col, std::string&& value, const Block& block) : m_name(name), m_line(line), m_col(col), 
+  m_type(block.m_types.at(CONST_CAT_NAMES[int(CONST_CAT::CC_STRING)]).get())
 {
-  if(!m_type) 
-    throw SemanticException
-    (
-      SEMANTIC_ERROR::SE_INVALID_TYPE,
-      std::format
-      (
-        "Semantic error: the type of the constant '{}' at ({},{}) is null ?",
-        m_name,m_line,m_col  
-      ),
-      m_line,m_col
-    );
-  
-  if constexpr (std::is_same_v<T,Int>)
+  m_val = std::move(value);
+  m_cat = CONST_CAT::CC_STRING;
+}
+
+template Const::Const(std::string_view name, size_t line, size_t col, Int value, const Block& block);
+template Const::Const(std::string_view name, size_t line, size_t col, Real value, const Block& block);
+template Const::Const(std::string_view name, size_t line, size_t col, char value, const Block& block);
+template Const::Const(std::string_view name, size_t line, size_t col, bool value, const Block& block);
+
+template <NumConstType T>
+Const::Const(std::string_view name, size_t line, size_t col, T value, const Block& block) : m_name(name), m_line(line), m_col(col),
+  m_type(block.m_types.at(CONST_CAT_NAMES[int(
+    (std::is_same_v<T, Int> ? CONST_CAT::CC_INT : 
+      (std::is_same_v<T, Real> ? CONST_CAT::CC_REAL : 
+        (std::is_same_v<T, char>) ? CONST_CAT::CC_CHAR : CONST_CAT::CC_BOOL
+      )
+    )
+  )]).get())
+{
+
+  m_val = value;
+
+  if constexpr (std::is_same_v<T, Int>)
   {
-    if (m_type->m_name != CONST_CAT_NAMES[int(CONST_CAT::CC_INT)])
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the type of the constant '{}' with value '{}' at ({},{}) should be '{}', not Int !",
-          m_name, value, m_line, m_col, m_type->m_name
-        ),
-        m_line, m_col
-      );
-    }
-    m_ival = value;
     m_cat = CONST_CAT::CC_INT;
   }
 
-  else if constexpr (std::is_same_v<T,Real>)
+  else if constexpr (std::is_same_v<T, Real>)
   {
-    if (m_type->m_name != CONST_CAT_NAMES[int(CONST_CAT::CC_REAL)])
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the type of the constant '{}' with value '{}' at ({},{}) should be '{}', not Real !",
-          m_name, value, m_line, m_col, m_type->m_name
-        ),
-        m_line, m_col
-      );
-    }
-    m_dval = value;
     m_cat = CONST_CAT::CC_REAL;
   }
 
-  else if constexpr (std::is_same_v<T,char>)
+  else if constexpr (std::is_same_v<T, char>)
   {
-    if (m_type->m_name != CONST_CAT_NAMES[int(CONST_CAT::CC_CHAR)])
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the type of the constant '{}' with value '{}' at ({},{}) should be '{}', not Char !",
-          m_name, value, m_line, m_col, m_type->m_name
-        ),
-        m_line, m_col
-      );
-    }
-    m_cval = value;
     m_cat = CONST_CAT::CC_CHAR;
   }
 
-  else if constexpr (std::is_same_v<T,std::string_view>)
+  else if constexpr (std::is_same_v<T, bool>)
   {
-    if (m_type->m_name != CONST_CAT_NAMES[int(CONST_CAT::CC_STRING)])
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the type of the constant '{}' with value '{}' at ({},{}) should be '{}', not String !",
-          m_name, value, m_line, m_col, m_type->m_name
-        ),
-        m_line, m_col
-      );
-    }
-    m_sval = value;
-    m_cat = CONST_CAT::CC_STRING;
-  }
-}
-
-template Int Const::get();
-template Real Const::get();
-template char Const::get();
-template std::string_view Const::get();
-
-template <ConstType T>
-T Const::get(){
-  if constexpr (std::is_same_v<T,Int>)
-  {
-    if (m_cat != CONST_CAT::CC_INT && m_cat != CONST_CAT::CC_ENUM)
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the constant '{}' with value '{}' at ({},{}) is neither an Int nor an Enum !",
-          m_name, m_ival, m_line, m_col
-        ),
-        m_line, m_col
-      );
-    }
-    return m_ival;
-  }
-
-  else if constexpr (std::is_same_v<T,Real>)
-  {
-    if (m_cat != CONST_CAT::CC_REAL)
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the constant '{}' with value '{}' at ({},{}) is not a Real !",
-          m_name, m_dval, m_line, m_col
-        ),
-        m_line, m_col
-      );
-    }
-    return m_dval;
-  }
-
-  else if constexpr (std::is_same_v<T,char>)
-  {
-    if (m_cat != CONST_CAT::CC_CHAR)
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the constant '{}' with value '{}' at ({},{}) is not a Char !",
-          m_name, m_cval, m_line, m_col
-        ),
-        m_line, m_col
-      );
-    }
-    return m_cval;
-  }
-
-  else if constexpr (std::is_same_v<T,std::string_view>)
-  {
-    if (m_cat != CONST_CAT::CC_STRING)
-    {
-      throw SemanticException
-      (
-        SEMANTIC_ERROR::SE_INVALID_TYPE,
-        std::format
-        (
-          "Semantic error: the constant '{}' with value '{}' at ({},{}) is not a String !",
-          m_name, m_sval, m_line, m_col
-        ),
-        m_line, m_col
-      );
-    }
-    return m_sval;
+    m_cat = CONST_CAT::CC_BOOL;
   }
 }
 
 Subrange::Subrange(
   const std::string_view& name, size_t line, size_t col,
   Const&& beg, Const&& end 
-) : Type(name, line, col), m_beg(std::move(beg)), m_end(std::move(end)) {
-  if(m_beg.m_cat != m_end.m_cat || typeid(m_beg.m_type) != typeid(m_end.m_type)){
+) : Type(name, line, col, TYPE_CAT::TC_SUBRANGE), m_beg(std::move(beg)), m_end(std::move(end)) {
+  if(m_beg.m_type != m_end.m_type){
     throw SemanticException(
       SEMANTIC_ERROR::SE_SUBRANGE_TYPES_MISMATCH,
       std::format(
@@ -228,10 +80,19 @@ Subrange::Subrange(
       m_beg.m_col
     );
   }
+
+  Int beg_value, end_value;
+
   switch(m_beg.m_cat){
     case CONST_CAT::CC_CHAR:
+      beg_value = std::get<char>(m_beg.m_val);
+      end_value = std::get<char>(m_end.m_val);
+      break;
     case CONST_CAT::CC_ENUM:
     case CONST_CAT::CC_INT:
+      beg_value = std::get<Int>(m_beg.m_val);
+      end_value = std::get<Int>(m_end.m_val);
+      break;
     break;
     default:
       throw SemanticException(
@@ -244,6 +105,120 @@ Subrange::Subrange(
         m_col
       );
   }
+
+  if (end_value <= beg_value){
+    throw SemanticException(
+      SEMANTIC_ERROR::SE_INVALID_SUBRANGE,
+      std::format(
+        "Semantic error: Subrange type \"{}\" 's beginning ({}) is greater or equals than its end ({}) !",
+        m_beg.m_type->to_string(), beg_value, end_value
+      ),
+      m_line,
+      m_col
+    );
+  }
+
 }
 
+void Enum::insert(const Lexeme& token, Block& block){
+  if(token.m_type != TOKEN_TYPE::ID_TOKEN){
+    throw SemanticException(
+      SEMANTIC_ERROR::SE_INVALID_TOKEN,
+      std::format(
+        "Semantic error: enum type '{}' values are identifiers. Found '{}' at ({},{}) !",
+        to_string(), token.m_id, token.m_line, token.m_col
+      ),
+      token.m_line,
+      token.m_col
+    );
+  }
+  if(m_values.contains(token.m_id)){
+    throw SemanticException(
+      SEMANTIC_ERROR::SE_DUPLICATE_ID,
+      std::format(
+        "Semantic error: enum type '{}' already contains value '{}'. Found earlier at ({},{}) !!",
+        to_string(), token.m_id, token.m_line, token.m_col
+      ),
+      token.m_line,
+      token.m_col
+    );
+  }
+  const Int value = Int(m_values.size());
+  m_values[token.m_id] = value;
+  block.m_enums_vals.emplace(std::make_pair(token.m_id, EnumValue(this, token, value)));
+}
+
+template <BlockMemberType T>
+inline const auto& getInfo(const T& t){
+  if constexpr (is_one_of<T, Label, Const, Var, EnumValue>){
+    return t;
+  }
+  else if constexpr(std::is_same_v<T, std::shared_ptr<Type>>){
+    return *t.get();
+  }
+  else if constexpr(std::is_same_v<T, Function>){
+    return *t.m_type;
+  }
+}
+
+template <BlockMemberType T>
+void Block::check_used_id(const Lexeme &token, const std::unordered_map<std::string_view, T> &map)
+{
+
+  if(auto it = map.find(token.m_id); it != map.end())
+  {
+
+    throw SemanticException(
+      SEMANTIC_ERROR::SE_DUPLICATE_ID,
+      std::format
+      (
+        "Semantic error: duplicate id '{}' found at ({},{}) and ({},{}) !",
+        token.m_id,
+        token.m_line,
+        token.m_col,
+        getInfo(it->second).m_line,
+        getInfo(it->second).m_col
+      ),
+      token.m_line,
+      token.m_col
+    );
+  }
+
+}
+
+void Block::check_used_id(const Lexeme& token)
+{
+  check_used_id(token, m_labels);
+  check_used_id(token, m_consts);
+  check_used_id(token, m_types);
+  check_used_id(token, m_vars);
+  check_used_id(token, m_funcs);
+  check_used_id(token, m_enums_vals);
+  Block *prev = m_parent;
+  while(prev){
+    check_used_id(token, prev->m_types);
+    prev = prev->m_parent;
+  }
+}
+
+template <BlockMemberType T>
+std::conditional_t<std::is_same_v<T, std::shared_ptr<Type>>, const Type*, const T*>
+Block::get(std::string_view id, const std::unordered_map<std::string_view, T> &map)
+{
+  const auto it = map.find(id);
+  if constexpr (std::is_same_v<T, std::shared_ptr<Type>>){
+    if(it != map.end()) return it->second.get();
+  }
+  else {
+    if(it != map.end()) return &(it->second);
+  }
+  return nullptr;
+}
+
+template const Type* Block::get(std::string_view, const std::unordered_map<std::string_view, std::shared_ptr<Type>>&);
+template const Label* Block::get(std::string_view, const std::unordered_map<std::string_view, Label>&);
+template const Const* Block::get(std::string_view, const std::unordered_map<std::string_view, Const>&);
+template const Var* Block::get(std::string_view, const std::unordered_map<std::string_view, Var>&);
+template const Function* Block::get(std::string_view, const std::unordered_map<std::string_view, Function>&);
+template const EnumValue* Block::get(std::string_view, const std::unordered_map<std::string_view, EnumValue> &);
 };
--- compiler/src/Semantics.hpp
+++ compiler/src/Semantics.hpp
@@ -5,6 +5,8 @@
 #include <unordered_map>
 #include <memory>
 #include <concepts>
+#include <variant>
+#include <unordered_set>
 
 namespace pascal_compiler
 {
@@ -13,7 +15,11 @@ enum class SEMANTIC_ERROR
 {
   SE_SUBRANGE_TYPES_MISMATCH,
   SE_DUPLICATE_ID,
-  SE_INVALID_TYPE
+  SE_INVALID_TYPE,
+  SE_INVALID_TOKEN,
+  SE_MISSING_ID,
+  SE_INVALID_OP,
+  SE_INVALID_SUBRANGE
 };
 
 class SemanticException{
@@ -59,59 +65,79 @@ enum class CONST_CAT : int
   CC_REAL,
   CC_ENUM,
   CC_STRING,
+  CC_BOOL,
   CC_CHAR,
   CC_MAX = CC_CHAR
 };
 
+template <typename T, typename... U>
+concept is_one_of = (std::is_same_v<T, U> || ... );
+
 template <typename T>
-concept ConstType = std::is_same_v<T,Int> || std::is_same_v<T,Real> || std::is_same_v<T,std::string_view> || std::is_same_v<T,char>;
+concept NumConstType = is_one_of<T, Int, Real, char, bool>;
 
 static inline const char* CONST_CAT_NAMES[int(CONST_CAT::CC_MAX) + 1] = {
   "Int",
   "Real",
   "Enum",
   "String",
+  "Bool",
   "Char"
 };
 
 struct Type;
-
 struct Enum;
+struct Block;
+struct EnumValue;
+struct Record;
+struct Function;
 
 struct Const
 {
-  
+  // A const doesn't always have an actaul name (when unamed, points to constant)
   std::string_view m_name;
   size_t m_line, m_col;
-  union {
-    Int m_ival;
-    Real m_dval;
-    std::string_view m_sval;
-    char m_cval;
-  };
-  Type *m_type;
+  std::variant<Int, Real, std::string, char, bool> m_val;
+  const Type *m_type; // Stored to tell which enum is used and to check types with variables
   CONST_CAT m_cat;
 
-  template <ConstType T>
-  Const(std::string_view name, size_t line, size_t col, T value, Type *type);
-
-  Const(std::string_view name, size_t line, size_t col, std::string_view value, Enum *type);
-
-  template <ConstType T>
-  T get();
+  template <NumConstType T>
+  Const(std::string_view name, size_t line, size_t col, T value, const Block& main_block);
+  Const(std::string_view name, size_t line, size_t col, std::string &&value, const Block &main_block);
+  Const(std::string_view name, size_t line, size_t col, const EnumValue& value);
 
 };
 
-//TODO: Types don't always have names, in which case we can create one instantaneously using a counter (since IDs cannot start with numbers). For now we pick ""
-//Remember that when type X = Y, X and Y are the same (same pointer)
+enum class TYPE_CAT : int{
+  TC_BASIC,
+  TC_ENUM,
+  TC_SUBRANGE,
+  TC_ARRAY,
+  TC_RECORD,
+  TC_FUNCTION,
+  TC_MAX = TC_FUNCTION
+};
+
+static inline const char* TYPE_CAT_NAMES[int(TYPE_CAT::TC_MAX) + 1] = {
+  "TC_BASIC",
+  "TC_ENUM",
+  "TC_SUBRANGE",
+  "TC_ARRAY",
+  "TC_RECORD",
+  "TC_FUNCTION"
+};
+
+//TODO: Types don't always have names, in which case we can store them as a unique_ptr for each scope (or block).
+//Remember that when type X = Y, X and Y are the same. Therefore, for named-types we use shared_ptr
 struct Type
 {
   const std::string_view m_name;
   const size_t m_line;
   const size_t m_col;
+  const TYPE_CAT m_type;
 
-  Type(std::string_view name, size_t line, size_t col) : m_name(name), m_line(line), m_col(col) {}
-  std::string to_string(){
+  Type(std::string_view name, size_t line, size_t col, TYPE_CAT type) : m_name(name), m_line(line), m_col(col), m_type(type) {}
+  std::string to_string() const{
     return std::format("\"{}\" at ({},{})", m_name, m_line, m_col); 
   }
 
@@ -120,8 +146,24 @@ struct Type
 
 struct Enum : Type
 {
-  std::unordered_map<std::string_view,int> m_values;
-  Enum(const std::string_view &name, size_t line, size_t col) : Type(name, line, col) {}
+  std::unordered_map<std::string_view,Int> m_values;
+  Enum(const std::string_view &name, size_t line, size_t col) : Type(name, line, col, TYPE_CAT::TC_ENUM) {}
+
+  // Automatically inserts EnumValue object to block
+  // Use only when Enum is a pointer (uses this internally). Clearly the Enum should outlive EnumValue, which exists to facilitate search 
+  void insert(const Lexeme&, Block&);
+};
+
+class EnumValue {
+  friend struct Enum;
+public:
+  const Enum* m_type;
+  const std::string_view m_id;
+  const Int m_value;
+  const size_t m_line;
+  const size_t m_col;
+private:
+  EnumValue(Enum* type, const Lexeme& token, Int value) : m_type(type), m_id(token.m_id), m_value(value), m_line(token.m_line), m_col(token.m_col) {} 
 };
 
 struct Subrange : Type
@@ -129,24 +171,24 @@ struct Subrange : Type
   Const m_beg, m_end; // Must contain the same type
   Subrange(
     const std::string_view& name, size_t line, size_t col,
-    Const&& beg, Const&& end 
+    Const&& beg, Const&& end
   );
 };
 
 struct Array : Type
 {
 
-  size_t m_size;
-  Type* m_etype;
+  std::vector<const Type*> m_itypes;  // index types
+  const Type* m_etype;                // element type
 
-  Array(const std::string_view &name, size_t line, size_t col) : Type(name, line, col) {}
+  Array(const std::string_view &name, size_t line, size_t col, std::vector<const Type*>&& types, const Type* etype) : 
+  Type(name, line, col, TYPE_CAT::TC_ARRAY), m_itypes(std::move(types)), m_etype(etype) {}
 };
-
-struct Record;
 
 struct Var
 {
   std::string_view m_name;
+  size_t m_line, m_col;
   Type *m_type;
 };
 
@@ -156,36 +198,51 @@ struct Record : Type
   std::unordered_map<std::string_view,Var> m_members;
   std::unordered_map<std::string_view,Record*> m_variants;
 
-  Record(const std::string_view &name, size_t line, size_t col) : Type(name, line, col) {}
+  Record(const std::string_view &name, size_t line, size_t col) : Type(name, line, col, TYPE_CAT::TC_RECORD) {}
 };
 
-struct Procedure;
-struct Function;
+struct FunctionType : Type
+{
+
+  std::vector<Var> m_args;
+  Type* m_ret_type; // Null for procedures
+
+  FunctionType(const std::string_view &name, size_t line, size_t col, Type* ret_type) : Type(name, line, col, TYPE_CAT::TC_FUNCTION), m_ret_type(ret_type) {}
+};
+
+template <typename T>
+concept BlockMemberType = is_one_of<T, Label, Const, std::shared_ptr<Type>, Var, Function, EnumValue>;
 
 struct Block
 {
   Block *m_parent;
   std::unordered_map<std::string_view, Label> m_labels;
   std::unordered_map<std::string_view, Const> m_consts;
-  std::unordered_map<std::string, std::unique_ptr<Type>> m_types;
+  std::unordered_map<std::string_view, std::shared_ptr<Type>> m_types;
   std::unordered_map<std::string_view, Var> m_vars;
-  std::unordered_map<std::string_view, Procedure> m_procs;
   std::unordered_map<std::string_view, Function> m_funcs;
+  std::unordered_map<std::string_view, EnumValue> m_enums_vals;
+
+  std::vector<std::unique_ptr<Type>> m_unamed_types;
 
   Block(Block *parent = nullptr) : m_parent(parent) {}
-};
 
-struct Procedure
-{
-  std::string_view m_name;
-  std::vector<Var> m_args;
-  std::unique_ptr<Block> m_block;
+  static void init_main_block(Block&);
+
+  // We plan to support variable shadowing including constants..., except for types 
+  template <BlockMemberType T>
+  static void check_used_id(const Lexeme &, const std::unordered_map<std::string_view, T> &);
+  void check_used_id(const Lexeme&);
+
+  template <BlockMemberType T>
+  static std::conditional_t<std::is_same_v<T, std::shared_ptr<Type>>, const Type*, const T*> 
+  get(std::string_view, const std::unordered_map<std::string_view, T> &);
 };
 
 struct Function
 {
-  Procedure m_proc;
-  Type *m_type;
+  FunctionType* m_type;
+  std::unique_ptr<Block> m_block;
 };
 
 }
